c3ee0550d595f4cbb28f0219e9875cc553ee41f1
James <redthunderspartan@gmail.com>
2024-11-22 19:07:06 -0600

    Commit Test Files

--- television.py
+++ television.py
@@ -10,13 +10,16 @@ class Television:
         self.__channel = self.MIN_CHANNEL
 
     def power(self):
+        """Toggles tv's power."""
         self.__status = not self.__status
 
     def mute(self):
+        """Toggles if the tv is muted"""
         if self.__status:
             self.__muted = not self.__muted
 
     def channel_up(self):
+        """Raises tv's channel up 1, if it is at the max channel it goes to min channel"""
         if self.__status:
             if self.__channel == self.MAX_CHANNEL:
                 self.__channel = self.MIN_CHANNEL
@@ -24,6 +27,7 @@ class Television:
                 self.__channel += 1
 
     def channel_down(self):
+        """Raises tv's channel down 1, if it is at the min channel it goes to max channel"""
         if self.__status:
             if self.__channel == self.MIN_CHANNEL:
                 self.__channel = self.MAX_CHANNEL
@@ -31,6 +35,7 @@ class Television:
                 self.__channel -= 1
 
     def volume_up(self):
+        """Raises tv's volume 1, if it reaches max it does not raise past"""
         if self.__status:
             if self.__muted:
                 self.mute()
@@ -38,7 +43,7 @@ class Television:
                 self.__volume += 1
 
     def volume_down(self):
-
+        """Lowers tv's volume 1, if it reaches min it does not lower past"""
         if self.__status:
             if self.__muted:
                 self.mute()
@@ -46,5 +51,9 @@ class Television:
                 self.__volume -= 1
 
     def __str__(self):
+        """
+        Method to show tv statsu
+        :return: tv status
+        """
         volume = self.__volume if not self.__muted else 0
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {volume}'
